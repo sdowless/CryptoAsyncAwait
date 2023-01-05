fb364fb2bd66fe845141b28831d4bc925857b993
Stephan Dowless <dowless.stephan@gmail.com>
2023-01-05 10:25:20 -0500

    Initial Commit

---
+++ CryptoAsyncAwait/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CryptoAsyncAwait
+//
+//  Created by Stephan Dowless on 1/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoAsyncAwait/CryptoAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAsyncAwaitApp.swift
+//  CryptoAsyncAwait
+//
+//  Created by Stephan Dowless on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
