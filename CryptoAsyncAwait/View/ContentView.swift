108548b6a81b568d10985da9f036d90903f8ad31
Stephan Dowless <dowless.stephan@gmail.com>
2023-01-05 11:20:35 -0500

    Completed project

--- CryptoAsyncAwait/View/ContentView.swift
+++ CryptoAsyncAwait/View/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var viewModel = ContentViewModel()
-    
+    @State private var showAlert = false
+
     var body: some View {
         NavigationStack {
             List {
                 ForEach(viewModel.coins) { coin in
                     CoinRowView(coin: coin)
+                        .onAppear {
+                            if coin.id == viewModel.coins.last?.id {
+                                viewModel.loadDataAsync()
+                            }
+                        }
                 }
+            }
+            .refreshable {
+                viewModel.handleRefresh()
+            }
+            .onReceive(viewModel.$error, perform: { error in
+                if error != nil {
+                    self.showAlert.toggle()
+                }
+            })
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(viewModel.error?.localizedDescription ?? "")
+                )
             }
             .navigationTitle("Live Prices")
         }
--- CryptoAsyncAwait/ViewModel/ContentViewModel.swift
+++ CryptoAsyncAwait/ViewModel/ContentViewModel.swift
@@ -9,15 +9,47 @@ import Foundation
 
 class ContentViewModel: ObservableObject {
     @Published var coins = [Coin]()
-    
+    @Published var error: Error?
+    private let fetchLimit = 20
+    private var page = 0
     let BASE_URL = "https://api.coingecko.com/api/v3/coins/"
     
     var urlString: String {
-        return  "\(BASE_URL)markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&price_change_percentage=24h"
+        return  "\(BASE_URL)markets?vs_currency=usd&order=market_cap_desc&per_page=\(fetchLimit)&page=\(page)&price_change_percentage=24h"
     }
     
     init() {
         fetchCoinsWithURLSession()
+    }
+    
+    func handleRefresh() {
+        coins.removeAll()
+        page = 0
+        loadDataAsync()
+    }
+}
+
+// MARK: - Async/Await
+
+extension ContentViewModel {
+    @MainActor
+    func fetchCoinsAsync() async throws {
+        page += 1
+        do {
+            guard let url = URL(string: urlString) else { throw CoinError.invalidURL }
+            let (data, response) = try await URLSession.shared.data(from: url)
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw CoinError.serverError }
+            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else { throw CoinError.invalidData }
+            self.coins.append(contentsOf: coins)
+        } catch {
+            self.error = error
+        }
+    }
+    
+    func loadDataAsync() {
+        Task(priority: .medium) {
+            try await fetchCoinsAsync()
+        }
     }
 }
 
